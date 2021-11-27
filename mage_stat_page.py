f953fda351101fac9d4df57e071d5fd4d146e09a
batmoose999 <batmoose999@gmail.com>
2021-11-27 11:38:34 -0500

    Adjusted mage stat formatt

--- mage_stat_page.py
+++ mage_stat_page.py
@@ -6,7 +6,7 @@ import sys
 from colors import *
 
 def mage_stat_function():
-    print("\n>Class: Mage\n>Stats<\n>Strength: 1\n>Inteligence: 4\n >Agility: 2")
+    print("\n>Class: Mage\n>Stats<\n>Strength: 1\n>Inteligence: 4\n>Agility: 2")
     print("\n>Information<\n>The Mage is a scholar dedicated to the study and practice of the magic arts.")
     print(">His strength is in the wave of his staff rather than the swinging of his sword.\n")
 
