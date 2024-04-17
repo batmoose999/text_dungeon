7366602e203cacaf2ccb5d8bb647cbf9ce8031a9
batmoose999 <batmoose999@gmail.com>
2024-04-17 15:04:46 -0400

    added stage one

--- stage_files/stage_one.py
+++ stage_files/stage_one.py
@@ -1,11 +1,15 @@
-#Alex Cummins
-#Text Dungeon Alpha
-#Mercenary Stat page
+# Alex Cummins
+# Text Dungeon Alpha
+# Mercenary Stat page
 
 import sys
 from colors import *
 
-def stage_one_function():
 
-    sys.stdout.write(RED)
-    print("\n>This is a placeholder for stage #1!")
+def stage_one_function():
+    sys.stdout.write(GREEN)
+    print("\n>Welcome to Stage One!<\n>This is the first stage of the game, and it will introduce you to the basic mechanics of the game.")
+    print(">You will start in a small room, with a door to the north and a door to the east.")
+    print(">To proceed, you must open one of the doors and enter the next room.")
+    print(">Be careful, though, as there may be enemies waiting for you on the other side.")
+    print(">Good luck!")
